17d9e21708e4d3c9bfc405749c85aef943c36d95
Jerry Uanino <juanino@gmail.com>
2020-05-16 22:49:46 -0400

    added wet/dry state for ifttt and 0/1 state for flask post

--- wifitest.ino
+++ wifitest.ino
@@ -12,14 +12,17 @@
 const char* devicename = "laundry1";
 const int water1 = 14; // water sensor plate on pin 14
 const int floodLed = 0; // builtin led on feather for alarm
+const int runningLed = 2; // used to visually confirm the program is running
 
 // variables
 int water1State = 0; // var for reading plate sensor #1
+int prior_w1state = 0; // placeholder for comparing state changes for sensor #1
 
 void setup() {
   // Water sensor setup 
   // initialize the LED pin as an output:
   pinMode(floodLed, OUTPUT);
+  digitalWrite(floodLed, HIGH);
   // init water plate as a input button
   pinMode(water1, INPUT);
   
@@ -27,7 +30,47 @@ void setup() {
   delay(100);
 
   // We start by connecting to a WiFi network
+  pinMode(runningLed, OUTPUT);
+  digitalWrite(runningLed, LOW);
+  setup_wifi();
+  digitalWrite(runningLed, HIGH);
+}
 
+int value = 0;
+int flash_counter = 0;
+
+// main loop
+void loop() {
+  // send your sensor data
+  check_water1();
+  flash();
+ 
+} // end main loop
+
+
+// Subroutines below here
+// ----------------------
+
+void flash() {
+  /* alternate the blue light so we know we haven't crashed
+   * this is better than a delay since we don't miss a sensor check
+   * last if block should be the sum of the two other if blocks to make
+   * even blink of on/off
+  */
+  if (flash_counter < 200) {
+    digitalWrite(runningLed, HIGH);
+  } 
+  if (flash_counter > 200) {
+    digitalWrite(runningLed, LOW);
+  } 
+  flash_counter++;
+  if (flash_counter == 400) {
+    flash_counter = 0;
+  }
+} // end flash routine
+
+void setup_wifi() {
+  // connect to SSID from config.h
   Serial.println();
   Serial.println();
   Serial.print("Connecting to ");
@@ -46,17 +89,9 @@ void setup() {
   Serial.println(WiFi.localIP());
 }
 
-int value = 0;
-
-void loop() {
-  // send your sensor data
-  check_water1();
- 
-} // end main loop
-
-void sendsensor() {
-  // here is where you should do some logic
-  // like read a sensor to send to the server
+void sendsensor(int status) {
+  Serial.print("you sent in a status of: " + String(status));
+  delay(1000);
   Serial.print("sendsensor: connecting to ");
   Serial.println(host);
   
@@ -74,7 +109,7 @@ void sendsensor() {
   Serial.println(url);
   
   // This will send the request to the server
-  client.print(String("GET ") + url + devicename + "/water1/1 " + "HTTP/1.1\r\n" +
+  client.print(String("GET ") + url + devicename + "/water1/" + String(status) + " HTTP/1.1\r\n" +
                "Host: " + host + "\r\n" + 
                "Connection: close\r\n\r\n");
 
@@ -91,25 +126,32 @@ void sendsensor() {
 
 void check_water1() {
   Serial.println("Checking water1 plate sensor.");
+  prior_w1state = water1State;
   water1State = digitalRead(water1); // read water sensor #1
+  if (prior_w1state == water1State) {
+    Serial.println("Same state as before:" + String(water1State));
+    return;
+  }
   // check if wet, because plate would be high #dank
   if (water1State == HIGH) {
     //turn LED on:
     digitalWrite(floodLed, LOW); // not sure why LOW is on for feather pin #0
     Serial.println("plate wet: light should go ON");
     delay(500);
-    send_ifttt();
+    send_ifttt("wet");
     delay(500);
-    sendsensor(); // call out to flask
+    sendsensor(1); // call out to flask 0=dry 1=wet
   } else {
     // turn LED off:
     digitalWrite(floodLed, HIGH);
     Serial.println("plate dry: light should go OFF");
+    sendsensor(0); // call out to flask 0=dry 1=wet
+    send_ifttt("dry");
     delay(500);
   }
 }
 
-void send_ifttt() {
+void send_ifttt(String status) {
   // here is where you should do some logic
   // like read a sensor to send to the server
   Serial.print("sendsensor: connecting to ");
@@ -127,14 +169,12 @@ void send_ifttt() {
   Serial.print("Requesting URL: ");
   Serial.println(url + "SECRET KEY"); // key is ifttt_key from config.h
   
-  // This will send the request to the server
-  Serial.println(String("GET ") + url + "SECRET KEY" + " HTTP/1.1\r\n" +
+  // This will send the HTTP request to the server
+  String full_url = (String("GET ") + url + ifttt_key + "?value1=" + status + " HTTP/1.1\r\n" +
                "Host: " + ifttt + "\r\n" + 
                "Connection: close\r\n\r\n");
-
-  client.print(String("GET ") + url + ifttt_key + " HTTP/1.1\r\n" +
-               "Host: " + ifttt + "\r\n" + 
-               "Connection: close\r\n\r\n");
+  Serial.println("trying: " + full_url);
+  client.print(full_url);
 
   delay(500);
   // Read all the lines of the reply from server and print them to Serial
